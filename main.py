b90b57e02d34486a42131fb3f9ce2f91895a905d
tarcisio bruni rangel <tarcisiobruni@hotmail.com>
2019-08-21 19:22:50 -0300

    refactor(aplicacao): Ajustes no arquivo de processamento dos dados, modularizando cada parte do algoritmo

--- main.py
+++ main.py
@@ -1,3 +1,41 @@
+# Estutura de Dados de uma Célula
+# (a,b,c,d)
+# a = coordenada na matriz
+# b = Custo de Deslocamento até o atual (1)
+# c = Distancia de Manhattan até o destino
+# d = coordenadas do ponto pai
+
+mapa = criaMapa()       # Cria matriz com o mapa passado em txt
+for i in mapa:
+    print(i)
+
+inicio = (0, 0)      # Inicio definido pelo professor
+final = (9, 8)       # Final definido pelo professor
+
+custo = 1            # g(x)
+listaAberta = []    # Lista de posições com as coordenadas que não tiveram seus vizinhos verificados
+listaFechada = []   # Lista de posições com as coordenadas que tiveram seus vizinhos verificados
+
+# Lista de posições que tiveram seus pesos calculados [((coordenada),1,17,(posOrigem)),...]
+listaPosicoesCalculadas = []
+
+def encontra_vizinhos(atual):
+    '''
+        vizinhos = 
+        # Vizinhos (linha , coluna)
+        (atual[0]-1) , (atual[1])   # Em cima
+        (atual[0]+1) , (atual[1])   # Em baixo
+        (atual[0]) , (atual[1]-1)   # Esquerda
+        (atual[0]) , (atual[1]+1)   # Direita
+    '''
+    return []
+
+def ordena_pelo_custo():
+    sorted(listaAberta, key=lambda t: (t[1]+t[2]))
+
+def calcula_custos(vizinho):
+    pass
+
 def criaMapa():
     matriz = []
 
@@ -10,42 +48,33 @@ def criaMapa():
 
     return matriz
 
-
-def distanciaManhattan(atual, final):   # Heuristica escolhida, distância em linha reta. h(x)
+# Heuristica escolhida, distância em linha reta. h(x)
+def distanciaManhattan(atual, final):
     return (final[0] - atual[0]) + (final[1] - atual[1])
-
 
 # Algoritmo A* : f(x) = g(x) + h(x)
 def main():
-    mapa = criaMapa()       # Cria matriz com o mapa passado em txt
-    for i in mapa:
-        print(i)
-
-    inicio = (0,0)      # Inicio definido pelo professor
-    final = (9,8)       # Final definido pelo professor
-
-    custo = 1            # g(x)
-    listaAberta = []    # Lista de posições que não tiveram seus vizinhos verificados
-    listaFechada = []   # Lista de posições que tiveram seus vizinhos verificados
-    dic = {}            # Dicionario de posições que tiveram seus pesos calculados {(0,0): [1,17]}
-
-    atual = inicio      # Começa no inicio
-    listaAberta.append(atual)     
+    listaAberta.append(inicio)
 
     while listaAberta != []:
-        #dic[atual] = (custo, distanciaManhattan(atual, final))
-
-        '''
-        vizinhos = 
-        # Vizinhos (linha , coluna)
-        (atual[0]-1) , (atual[1])   # Em cima
-        (atual[0]+1) , (atual[1])   # Em baixo
-        (atual[0]) , (atual[1]-1)   # Esquerda
-        (atual[0]) , (atual[1]+1)   # Direita
-        '''
-
+        # primeiro elemento da lista já ordenada
+        atual = listaAberta[0]
+        
+        # Pesquisa pelos elementos vizinhos elegiveis (nao é barreira) e not in ListaFechada
+        vizinhos = encontra_vizinhos(atual)
+        
+        listaAberta.extend(vizinhos)  # Adição desses elementos à lista
+        calcula_custos(vizinhos)  # Calculo de custo de cada vizinho
+        
+        # Remocao do primeiro elemento da lista aberta
+        listaAberta.remove(atual)
+        
+        # Adicionando o elemento processado na lista fechada
+        listaFechada.append(atual)
+        
+        # Ordenação dos elementos em ordem de custo crescente
+        ordena_pelo_custo()       
     return 0
-
 
 if __name__ == "__main__":
     main()
